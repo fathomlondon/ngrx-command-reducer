8e6af35222db862ce0c0d13501923fe2824d116f
Philip Bulley <philip@milkisevil.com>
2017-01-13 16:55:27 +0000

    Allows replacement of commands via useMockCommand. 1.1.0

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # ngrx-command-reducer
 
-A command-style redux reducer for Angular 2 and [ngrx/store](https://github.com/ngrx/store) with type checking for actions and their payloads.
+A command-style redux reducer for Angular and [ngrx/store](https://github.com/ngrx/store) with type checking for actions
+and their payloads.
 
 * Reducers essentially become a clean map of actions to pure function commands. 
 * The highly-testable commands encapsulate your business logic and return state.
 * Actions are class based (no more magic strings!) and have typed payloads.
 * State and action payloads are type checked against the commands you map them to.
+* Highly testable
 
 ## Installation
 
@@ -33,8 +35,8 @@ const DEFAULT_STATE: CurrentUser = {
 
 export const currentUserReducer: ActionReducer<CurrentUser> = new CommandReducer<CurrentUser>(DEFAULT_STATE)
   .add(LoginAction, loginCommand)
-  .add(LoginSuccessAction, loginSuccessCommand)
-  .add(LoginErrorAction, loginErrorCommand)
+  .add(LoginSuccessAction, loginSuccessReducerCommand)
+  .add(LoginErrorAction, loginErrorReducerCommand)
   .reducer();
 ```
 
@@ -47,20 +49,20 @@ import { LoginParams } from './login-params';
 
 export class LoginAction implements Action {
   /**
-   * Assinging a meaningful value to the `type` property is not 
+   * Assigning a meaningful value to the `type` property is not 
    * mandatory. It's only required for guaranteed compatibility with any 
    * third-party ngrx tools not using an `instanceof` check for actions.
    */
   type: string = 'LoginAction';
 
-  constructor(public payload?: LoginParams) {
+  constructor(public payload: LoginParams) {
     //
   }
 }
 ```
 
 
-### Command
+### ReducerCommand
 
 ```js
 import { LoginParams } from './login-params';
@@ -80,7 +82,8 @@ export function loginCommand(state: CurrentUser, payload: LoginParams): CurrentU
 
 ### Effects
 
-If using [@ngrx/effects](https://github.com/ngrx/effects), you can respond to a typed action by replacing the `.ofType` operator with `.filter`:
+If using [@ngrx/effects](https://github.com/ngrx/effects), you can respond to a typed action by replacing the `.ofType` 
+operator with `.filter`:
 
 ```js
 @Effect() login$ = this.actions$
@@ -99,4 +102,38 @@ const password = 'ilikebar';
 
 // The LoginAction payload is defined by the LoginParams interface
 this.store$.dispatch(new LoginAction({username, password}));
+```
+
+### Testing
+
+#### ReducerCommands
+
+`ReducerCommands` are extremely easy to test, as they should be implemented as pure functions. 
+
+1. Import the pure function into your test spec
+1. Pass a mock state and payload when manually executing the reducer command
+1. Assert on the returned state
+
+#### Reducers
+
+You can also test that the `CommandReducer` is executing the correct command functions when specific actions are 
+dispatched. Simply mock your commands with `useMockCommand()` like so:
+ 
+_foo.reducer.ts:_
+
+```js
+export const fooReducer: ActionReducer<Foo> = new CommandReducer<Foo>(DEFAULT_STATE)
+  .add(AddBarAction, addBarReducerCommand)
+  .reducer();
+```
+    
+_foo.reducer.spec.ts — an example using jasmine:_
+
+```js
+const myMockCommand = jasmine.createSpy('myMockCommand');
+fooReducer.useMockCommand(AddBarAction, myMockCommand);
+
+// ... dispatch AddBarAction to store ...
+
+expect(myMockCommand).toHaveBeenCalled();
 ```
--- command-reducer.ts
+++ command-reducer.ts
@@ -1,36 +1,125 @@
 import { Action, ActionReducer } from '@ngrx/store';
 
-interface ActionMapping<A, P> {
+interface StaticAction<A, P> {
 	new(...args: any[]): A;
 	prototype: {payload?: P};
 }
 
-interface ActionCommandMapping<S, P> {
+export interface ReducerCommand<S, P> {
 	(state: S, payload: P): S;
 }
 
+export interface CommandReducerMapping {
+	action: StaticAction<Action, any>;
+	command: ReducerCommand<any, any>;
+}
+
+declare module '@ngrx/store' {
+	interface ActionReducer<S> {
+		useMockCommand?<P>(command: ReducerCommand<S, P>, mockCommand: ReducerCommand<S, P>): CommandReducerMapping[];
+	}
+}
+
+/**
+ * A command-style redux reducer for Angular and [ngrx/store](https://github.com/ngrx/store) with type checking for
+ * actions and their payloads.
+ *
+ * @example
+ *
+ *     // add-bar.reducer-command.ts
+ *     export function addBarReducerCommand: ReducerCommand<FooState, Bar> = (
+ *         foo: FooState, payload: Bar
+ *     ): Favorites => {
+ *         foo = {...foo};
+ *         foo.bar = payload;
+ *
+ *         return foo;
+ *     }
+ *
+ *     // foo.reducer.ts
+ *     export const fooReducer: ActionReducer<FooState> =
+ *         new CommandReducer<FooState>(DEFAULT_STATE)
+ *         .add(AddBarAction, addBarReducerCommand)
+ *         .add(AddZarAction, addZarReducerCommand)
+ *         .reducer();
+ */
 export class CommandReducer<S> {
-	private map = [];
+	private map: CommandReducerMapping[] = [];
 
 	constructor(private defaultState: S) {
-		//
+		// Make `useMockCommand` easily accessible when only exporting the `ActionReducer` from a module
+		this._reducer.useMockCommand = this.useMockCommand;
 	}
 
-	add = <A extends Action, P>(action: ActionMapping<A, P>, command: ActionCommandMapping<S, P>) => {
+	/**
+	 * Adds a `ReducerCommand` function that will be executed when the store receives the specified action.
+	 *
+	 * Actions and ReducerCommands are many-to-many, so you may use any combination, as many times as you like as long
+	 * as the `ReducerCommand` function signature is compatible with the `ActionReducer`'s state and the `Action`'s
+	 * payload.
+	 *
+	 * @param action
+	 * @param command
+	 * @chainable
+	 */
+	add = <P>(action: StaticAction<Action, P>, command: ReducerCommand<S, P>): CommandReducer<S> => {
 		this.map.push({action, command});
 
 		return this;
 	};
 
+	/**
+	 * Returns the `ActionReducer` to be used within `@ngrx/store`.
+	 */
 	reducer = (): ActionReducer<S> => this._reducer;
 
+	/**
+	 * Replaces each occurance of a `ReducerCommand` with a mock `ReducerCommand`. Ideal for spying on command execution
+	 * in test scenarios.
+	 *
+	 * Accessible as a method on the returned `ActionReducer` instance.
+	 *
+	 * @example
+	 *
+	 *     // foo.reducer.ts
+	 *     export const fooReducer: ActionReducer<Foo> =
+	 *         new CommandReducer<Foo>(DEFAULT_STATE)
+	 *         .add(AddBarAction, addBarReducerCommand)
+	 *         .reducer();
+	 *
+	 *     // foo.reducer.spec.ts - an example using jasmine
+	 *     const myMockCommand = jasmine.createSpy('myMockCommand');
+	 *     fooReducer.useMockCommand(AddBarAction, myMockCommand);
+	 *     // ... dispatch AddBarAction to store ...
+	 *     expect(myMockCommand).toHaveBeenCalled();
+	 *
+	 * @param command       The command you want to replace
+	 * @param mockCommand   The command you want to use in it's place.
+	 * @returns An array of affected `CommandReducerMapping` objects.
+	 */
+	private useMockCommand = <P>(
+		command: ReducerCommand<S, P>, mockCommand: ReducerCommand<S, P>
+	): CommandReducerMapping[] => {
+		return this.map
+			.filter((mapping: CommandReducerMapping) => {
+				if (mapping.command === command) {
+					mapping.command = mockCommand;
+					return true;
+				}
+				return false;
+			});
+	};
+
+	/**
+	 * The `ActionReducer`. To be accessed via `reducer()`.
+	 */
 	private _reducer: ActionReducer<S> = (state: S, action: Action): S => {
 		if (typeof state === 'undefined') {
 			state = this.defaultState;
 		}
 
 		return this.map.reduce(
-			(prevState, mapping) => action instanceof mapping.action
+			(prevState: S, mapping: CommandReducerMapping) => action instanceof mapping.action
 				? mapping.command(prevState, action.payload)
 				: prevState,
 			state
